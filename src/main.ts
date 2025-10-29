1c7829f9daf2771979d60b35191587bfa0069e05
Anastasia Yarosh <fysyad@gmail.com>
2025-10-29 10:34:31 +0300

    Подключение PrimeVue

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,17 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
 app.mount('#app')
