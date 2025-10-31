95a94622b8818aee861173ce0e1661a8ee6997e6
Anastasia Yarosh <fysyad@gmail.com>
2025-10-31 13:37:05 +0300

    Логику вынесла в композабл

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,30 @@
 <script setup lang="ts">
-import DataTable, { type DataTableSortEvent } from 'primevue/datatable'
+import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
 import Dialog from 'primevue/dialog'
 import RadioButton from 'primevue/radiobutton'
 import Button from 'primevue/button'
 import InputText from 'primevue/inputtext'
 import Slider from 'primevue/slider'
-import { ref, computed } from 'vue'
-import { usersData } from './data/users'
-import type { User } from './types/user'
+import { useUsers } from './composables/useUsers'
 
-const users = ref(usersData.users)
-const initialUserState = ref([...usersData.users]) // создаем копию массива
-
-const sortField = ref<string | ((item: number | string) => string) | undefined>('id')
-const sortOrder = ref<number>(1)
-
-const onSort = (event: DataTableSortEvent) => {
-  sortField.value = event.sortField
-  sortOrder.value = event.sortOrder ?? 1
-}
-
-const visible = ref(false)
-
-const selectedUser = ref<User | null>(null)
-
-const onIdCellClick = (Id: string) => {
-  const user = usersData.users.find((item) => item.id === Id)
-  if (user) {
-    selectedUser.value = user
-    visible.value = true
-  }
-}
-
-const gender = ref('')
-const firstName = ref('')
-const lastName = ref('')
-const email = ref('')
-const ageRange = ref<[number, number]>([20, 50])
-
-// Вычисляем минимальный и максимальный возраст для слайдера
-const minAge = computed(() => Math.min(...users.value.map((user) => parseInt(user.age))))
-const maxAge = computed(() => Math.max(...users.value.map((user) => parseInt(user.age))))
-
-const filteredUsers = computed(() => {
-  return users.value.filter((user) => {
-    const userAge = parseInt(user.age)
-
-    // Проверяем фильтр по возрасту
-    const ageMatch = userAge >= ageRange.value[0] && userAge <= ageRange.value[1]
-
-    // Проверяем фильтр по полу
-    const genderMatch = !gender.value || user.gender.toLowerCase() === gender.value.toLowerCase()
-
-    // Проверяем фильтр по имени
-    const firstNameMatch =
-      !firstName.value || user.first_name.toLowerCase().includes(firstName.value.toLowerCase())
-
-    // Проверяем фильтр по email
-    const emailMatch = !email.value || user.email.toLowerCase().includes(email.value.toLowerCase())
-
-    // Проверяем фильтр по фамилии
-    const lastNameMatch =
-      !lastName.value || user.last_name.toLowerCase().includes(lastName.value.toLowerCase())
-
-    // Возвращаем пользователя только если ВСЕ условия выполняются
-    return ageMatch && genderMatch && firstNameMatch && emailMatch && lastNameMatch
-  })
-})
-
-const resetSort = () => {
-  users.value = [...initialUserState.value]
-  sortField.value = 'id'
-  sortOrder.value = 1
-  gender.value = ''
-  firstName.value = ''
-  lastName.value = ''
-  email.value = ''
-  ageRange.value = [minAge.value, maxAge.value] // сбрасываем слайдер к исходным значениям
-}
+const {
+  resetSort,
+  filteredUsers,
+  onIdCellClick,
+  onSort,
+  visible,
+  email,
+  gender,
+  firstName,
+  lastName,
+  ageRange,
+  maxAge,
+  minAge,
+  selectedUser,
+  sortField,
+  sortOrder,
+} = useUsers()
 </script>
 
 <template>
---
+++ src/composables/useUsers.ts
@@ -0,0 +1,93 @@
+import { ref, computed } from 'vue'
+import { usersData } from '../data/users'
+import type { User } from '../types/user'
+import { type DataTableSortEvent } from 'primevue/datatable'
+export function useUsers() {
+  const users = ref(usersData.users)
+  const initialUserState = ref([...usersData.users]) // создаем копию массива
+
+  const sortField = ref<string | ((item: number | string) => string) | undefined>('id')
+  const sortOrder = ref<number>(1)
+
+  const onSort = (event: DataTableSortEvent) => {
+    sortField.value = event.sortField
+    sortOrder.value = event.sortOrder ?? 1
+  }
+
+  const visible = ref(false)
+
+  const selectedUser = ref<User | null>(null)
+
+  const onIdCellClick = (Id: string) => {
+    const user = usersData.users.find((item) => item.id === Id)
+    if (user) {
+      selectedUser.value = user
+      visible.value = true
+    }
+  }
+
+  const gender = ref('')
+  const firstName = ref('')
+  const lastName = ref('')
+  const email = ref('')
+  const ageRange = ref<[number, number]>([20, 50])
+
+  // Вычисляем минимальный и максимальный возраст для слайдера
+  const minAge = computed(() => Math.min(...users.value.map((user) => parseInt(user.age))))
+  const maxAge = computed(() => Math.max(...users.value.map((user) => parseInt(user.age))))
+
+  const filteredUsers = computed(() => {
+    return users.value.filter((user) => {
+      const userAge = parseInt(user.age)
+
+      // Проверяем фильтр по возрасту
+      const ageMatch = userAge >= ageRange.value[0] && userAge <= ageRange.value[1]
+
+      // Проверяем фильтр по полу
+      const genderMatch = !gender.value || user.gender.toLowerCase() === gender.value.toLowerCase()
+
+      // Проверяем фильтр по имени
+      const firstNameMatch =
+        !firstName.value || user.first_name.toLowerCase().includes(firstName.value.toLowerCase())
+
+      // Проверяем фильтр по email
+      const emailMatch =
+        !email.value || user.email.toLowerCase().includes(email.value.toLowerCase())
+
+      // Проверяем фильтр по фамилии
+      const lastNameMatch =
+        !lastName.value || user.last_name.toLowerCase().includes(lastName.value.toLowerCase())
+
+      // Возвращаем пользователя только если ВСЕ условия выполняются
+      return ageMatch && genderMatch && firstNameMatch && emailMatch && lastNameMatch
+    })
+  })
+
+  const resetSort = () => {
+    users.value = [...initialUserState.value]
+    sortField.value = 'id'
+    sortOrder.value = 1
+    gender.value = ''
+    firstName.value = ''
+    lastName.value = ''
+    email.value = ''
+    ageRange.value = [minAge.value, maxAge.value] // сбрасываем слайдер к исходным значениям
+  }
+  return {
+    resetSort,
+    filteredUsers,
+    onIdCellClick,
+    onSort,
+    visible,
+    email,
+    gender,
+    firstName,
+    lastName,
+    ageRange,
+    maxAge,
+    minAge,
+    selectedUser,
+    sortField,
+    sortOrder,
+  }
+}
